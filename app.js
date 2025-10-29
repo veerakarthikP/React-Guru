e8ede2124e89fec8fb5bc29b7ba4458431008106
Karthik <karthik789393@gmail.com>
2025-10-30 00:23:29 +0530

    Laying the foundatation

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello React from React!..............."
-);
+const jsxHeading = <h1>Hello React from JSX!</h1>;
+
+const FunctionalComponent = () => {
+  return (
+    <div>
+      {jsxHeading}
+      <h1>Hello React from FunctionalComponent!</h1>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<FunctionalComponent />);
