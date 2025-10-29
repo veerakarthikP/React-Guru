8e5b97be2b56b711e2c9bc32c7508e16060077c7
Karthik <karthik789393@gmail.com>
2025-10-30 00:07:50 +0530

    Igniting our App

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
   "h1",
   { id: "heading" },
-  "Hello React from React!"
+  "Hello React from React!..............."
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
