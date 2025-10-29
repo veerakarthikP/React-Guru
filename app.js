d6e6fb8bcda7d956a030e5ce7d7827c294a14607
Karthik <karthik789393@gmail.com>
2025-10-29 23:38:42 +0530

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const heading = React.createElement(
   "h1",
   { id: "heading" },
-  "Hello React from React"
+  "Hello React from React!"
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
